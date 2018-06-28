367568616dfe74f5bc607fab363cd6e1556a71b6
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2018-06-28 21:52:16 +0200

    forms

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter } from '../actions/filters';
+import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
     <div>
         <input type="text" value={props.filters.text} onChange={(e) => {
             props.dispatch(setTextFilter(e.target.value));
         }} />
+        <select
+            value={props.filters.sortBy}
+            onChange={(e) => {
+
+                const value = e.target.value;
+                switch (value) {
+                    case 'date':
+                        props.dispatch(sortByDate());
+                        break;
+                    case 'amount':
+                        props.dispatch(sortByAmount());
+                        break;
+                }
+
+            }}>
+            <option value="date">Date</option>
+            <option value="amount">Amount</option>
+        </select>
     </div>
 );
 
