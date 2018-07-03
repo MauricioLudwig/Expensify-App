f3fb4f4463ce598c044d39f928dbe8b46fb9f758
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2018-07-03 07:30:51 +0200

    testing expenses selectors

---
+++ src/tests.js/actions/filters.test.js
@@ -0,0 +1,54 @@
+import moment from 'moment';
+import {
+    setStartDate,
+    setEndDate,
+    sortByDate,
+    sortByAmount,
+    setTextFilter
+} from '../../actions/filters';
+
+test('should generate set start date action object', () => {
+    const action = setStartDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    });
+});
+
+test('should generate set end date action object', () => {
+    const action = setEndDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_END_DATE',
+        endDate: moment(0)
+    });
+});
+
+test('should set text to submitted value', () => {
+    const action = setTextFilter('My Test');
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: 'My Test'
+    });
+});
+
+test('should set text to default', () => {
+    const action = setTextFilter();
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: ''
+    });
+});
+
+test('should generate sort by date action object', () => {
+    const action = sortByDate();
+    expect(action).toEqual({
+        type: 'SORT_BY_DATE'
+    });
+});
+
+test('should generate sort by amount action object', () => {
+    const action = sortByAmount();
+    expect(action).toEqual({
+        type: 'SORT_BY_AMOUNT'
+    });
+});
---
+++ src/tests.js/selectors/expenses.test.js
@@ -0,0 +1,45 @@
+import moment from 'moment';
+import selectExpenses from '../../selectors/expenses';
+
+const expenses = [{
+    id: '1',
+    description: 'Gum',
+    note: '',
+    amount: 195,
+    createdAt: 0
+}, {
+    id: '2',
+    description: 'Rent',
+    note: '',
+    amount: 109500,
+    createdAt: moment(0).subtract(4, 'days').valueOf()
+}, {
+    id: '3',
+    description: 'Credit Card',
+    note: '',
+    amount: 4500,
+    createdAt: moment(0).add(4, 'days').valueOf()
+}];
+
+test('should filter by text value', () => {
+    const filters = {
+        text: 'e',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([expenses[2], expenses[1]])
+});
+
+test('should filter by startDate', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: moment(0),
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([expenses[2], expenses[0]]);
+});
+
