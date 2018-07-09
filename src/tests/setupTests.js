ac2061d0e2025d4a2212c52ba643f640292a453f
MauricioLudwig <mrdamiengallagher@gmail.com>
2018-07-09 21:30:23 +0200

    config changes

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,7 +1,6 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import DotEnv from 'dotenv';
-DotEnv.config({ path: '.env.test' });
+require('dotenv').config({ path: '.env.test' });
 
 Enzyme.configure({
     adapter: new Adapter()
